d1b4f6792b904e602f5208f65f446fe31c7860b0
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2011-11-16 17:51:56 -0600

    initial commit

---
+++ ICalSplit.groovy
@@ -0,0 +1,88 @@
+
+
+class ICalSplit {
+
+	static def maxEventsPerFile = 1000
+
+	static void main(String[] args) {
+
+		if (args) {
+			def fileName = args[0]
+			def inputFile = new File(fileName)
+
+			if (inputFile.exists()) {
+				println "Splitting file: ${fileName}"
+
+				splitFile(inputFile, maxEventsPerFile)
+
+			} else {
+				println "File not found: ${fileName}"
+				displayHelp()
+			}
+		} else {
+			displayHelp()
+		}
+	}
+
+	static void displayHelp () {
+		println """
+		Usage: groovy ICalSplit.groovy [yourcalendar.ics]
+
+		This program splits an iCal file in to chunks of ${maxEventsPerFile} events each
+		"""
+	}
+
+	static File getSplitFile(File inputFile, splitNumber) {
+		new File(inputFile.name + '.' + splitNumber.toString().padLeft(3,'0'))
+	}
+
+	static void splitFile(File inputFile, Integer eventsPerSplit) {
+		def header = new StringBuffer()
+		def footer = 'END:VCALENDAR'
+		def eventCount = 0
+		def eventSplit = 0
+		def splitNumber = 1
+		def splitFileChunk = getSplitFile(inputFile, splitNumber)
+		println "Writing ${splitFileChunk}"
+
+		inputFile.eachLine{ line ->
+			// increment our event counter if needed
+			if ( line == 'BEGIN:VEVENT' ) {
+				// append the header to the split file
+				if (eventCount == 0) {
+					splitFileChunk << header.toString()
+				}
+				eventCount++
+				eventSplit++
+			}
+
+			// see if we've hit our quota for this chunk yet.
+			if (eventSplit > eventsPerSplit) {
+				// reset the split counter
+				eventSplit = 1
+				// increment the split number
+				splitNumber++
+				// write the footer to the split file
+				splitFileChunk << footer
+				// get a new split file
+				splitFileChunk = getSplitFile(inputFile, splitNumber)
+				println "Writing ${splitFileChunk}"
+				// write the header to the split file
+				splitFileChunk << header.toString()
+			}
+
+			// check to see if we've gotten to any events yet.
+			if ( eventCount == 0 ) {
+				// append to the header if we're still in it
+				header.append(line + '\n')
+			} else {
+				// append to the split file
+				splitFileChunk << line + '\n'
+			}
+		}
+
+		// write the footer to the split file
+		splitFileChunk << footer
+
+	}
+}
